0840682fcce9581cd0f6b46769aa0f6f4bb53494
Marcus Vinicius 07 <marcus.vinicius.16041997@gmail.com>
2023-08-30 22:45:03 -0300

    .

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -5,6 +5,13 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent: 200, childAspectRatio: 3 / 2, crossAxisSpacing: 20, mainAxisSpacing: 20));
+    return GridView(
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+    );
   }
 }
